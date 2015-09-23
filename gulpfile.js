e670dbf62c1263eb860f8fc3e9bd69551d4c973a
Chris Randle <crandle@DX1T6LM02.digitalglobe.com>
2015-09-23 16:04:27 -0600

    Initial commit

---
+++ gulpfile.js
@@ -0,0 +1,46 @@
+(function() {
+    'usei strict';
+    var gulp = require('gulp'),
+    nodemon = require('gulp-nodemon'),
+    watch = require('gulp-watch'),
+    jshint = require('gulp-jshint'),
+    livereload = require('gulp-livereload'),
+    plumber = require('gulp-plumber'),
+    _paths = ['server/**/*.js', 'client/js/*.js', 'client/dist/**/*.html];
+
+    //register nodemon task
+    gulp.task('nodemon', function() {
+        nodemon({
+            script: 'server/app.js',
+            env: {
+                'NODE_ENV': 'development'
+            }
+        })
+        .on('restart');
+    });
+
+    // Rerun the task when a file changes
+    gulp.task('watch', function() {
+        livereload.listen();
+        gulp.src(_paths, {
+            read: false
+        })
+        .pipe(plumber())
+        .pipe(jshint())
+        .pipe(jshint.reporter('default'))
+        .pipe(plumber.stop());
+        watch(_paths, livereload.changed);
+    });
+
+    //lint js files
+    gulp.task('lint', function() {
+        gulp.src(_paths)
+        .pipe(jshint())
+        .pipe(jshint.reporter('default'));
+    });
+
+    // The default task (called when you run `gulp` from cli)
+    // gulp.task('default', ['lint', 'nodemon', 'watch']); // TODO: restrict lint to our code
+    gulp.task('default', ['nodemon', 'watch']);
+}());
+
